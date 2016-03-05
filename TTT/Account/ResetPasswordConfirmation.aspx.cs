5ecc37188a53545409d159325b47d20138acdb31
jungmtg <cardofang@gmail.com>
2016-03-05 17:23:01 +0800

    Inital

---
+++ TTT/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace TTT.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ TTT/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TTT.Global" Language="C#" %>
